dec73cbd0fa80479652287182499749d2bd080d5
Samin Islam <saminsss@gmail.com>
2024-12-14 18:40:37 -0800

    add theming

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -6,8 +6,12 @@ import Image from "next/image";
 import logo from "@/assets/logo.png";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
+import ThemeToggle from "@/components/ThemeToggle";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
+  const { theme } = useTheme();
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -21,24 +25,28 @@ const Navbar = () => {
           />
           <span className="text-xl font-bold tracking-tight">Mortfolio</span>
         </Link>
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: {
-                width: 35,
-                height: 35,
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 35,
+                  height: 35,
+                },
               },
-            },
-          }}
-        >
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Billing"
-              labelIcon={<CreditCard className="size-4" />}
-              href="/billing"
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                labelIcon={<CreditCard className="size-4" />}
+                href="/billing"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
       </div>
     </header>
   );
